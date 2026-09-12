50d2ca2e87419b88b7418f8dcde1ff0b35514a5f
Daxfgr <daxfgr123@gmail.com>
2026-09-12 10:37:27 -0300

    Fix RipperSpawn sound settings and playback
    Updated sound properties for RipperSpawn.

--- Spawner/RipperSpawn.lua
+++ Spawner/RipperSpawn.lua
@@ -1,30 +1,27 @@
 local sound = Instance.new("Sound")
-sound.Name = "RipperSpawn"
+sound.Name = "RipperSpawm"
 sound.Parent = workspace
-
 sound.AcousticSimulationEnabled = true
 sound.Looped = false
 sound.PlayOnRemove = false
-sound.PlaybackSpeed = 1
+sound.PlaybackSpeed = 0.6
 sound.PlaybackRegionsEnabled = false
 sound.SoundId = "rbxassetid://9125713501"
-sound.TimePosition = 0
-sound.Volume = 1
-sound.RollOffMaxDistance = 150
-sound.RollOffMinDistance = 0
+sound.TimePosition = 5.596
+sound.Volume = 6
+sound.RollOffMaxDistance = 50
+sound.RollOffMinDistance = 1
 sound.RollOffMode = Enum.RollOffMode.InverseTapered
 
 local pitchShift = Instance.new("PitchShiftSoundEffect")
 pitchShift.Name = "RipperSpawn"
-pitchShift.Octave = 1.5
+pitchShift.Octave = 0.875
 pitchShift.Enabled = true
 pitchShift.Priority = 0
 pitchShift.Parent = sound
 
 sound.Ended:Connect(function()
-	if sound.Parent then
-		sound:Destroy()
-	end
+	sound:Destroy()
 end)
 
 sound:Play()
